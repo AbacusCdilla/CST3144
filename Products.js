51c9958c1a8cba370099f9887e2cc1b3c9ce4f68
Sharjeel <m.sharjeel.zahid@gmail.com>
2024-10-20 20:07:39 +0400

    Updating

---
+++ Products.js
@@ -0,0 +1,12 @@
+const allProducts = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+        price: 2000,
+        image: "E:/MDX/Symester 1/Full Stack Development, Module CST3144/Week4/Checkout/imgs/cat-food.png",
+        availableInventory: 10,
+        rating: 3
+    },
+    // Additional products...
+];
